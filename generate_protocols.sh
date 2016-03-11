24330abb20aea633ee72e59857e3bf183d4d61cb
Denis Lavrov <bahus.vel@gmail.com>
2016-03-12 10:11:24 +1300

    Refactoring after server separation

--- generate_protocols.sh
+++ generate_protocols.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # for swift:    https://github.com/alexeyxo/protobuf-swift
 # for go:       https://github.com/golang/protobuf
-protoc --go_out=vmail/ vmail_proto/*.proto
-protoc --swift_out=vmail_swift/ vmail_proto/*.proto
+protoc --go_out=../vproto/ *.proto
+#protoc --swift_out=../vswift/ vmail_proto/*.proto
